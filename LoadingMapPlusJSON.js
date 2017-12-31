8b068f27b64e2e74eb4b8c80fb6390ccb4682633
avacadoadam <avacadoadam@gmail.com>
2017-12-31 05:30:51 +0000

    Initial commit

---
+++ LoadingMapPlusJSON.js
@@ -0,0 +1,214 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 2,
+        styles:[
+            {
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "saturation": 36
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 40
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    },
+                    {
+                        "weight": 1.2
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 21
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 29
+                    },
+                    {
+                        "weight": 0.2
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 18
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 19
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            }
+        ]
+
+    });
+
+
+    //Function to add marker and info on click
+    function AddMarker(NewWatch){
+        var marker = new google.maps.Marker({
+            position:NewWatch.Coord,
+            map:map
+
+            //icon:''to chaneg marker
+        });
+        if(NewWatch.icon){
+            marker.setIcon(NewWatch.icon);
+        }
+        var infoWindow = new google.maps.InfoWindow({
+            content: NewWatch.Header
+        });
+
+        marker.addListener('click',function () {
+            infoWindow.open(map,marker);
+        })
+    }
+
+
+    var json;
+    //Get JSON FILE AND PARSE INTO OBJECT CALLLED Responce
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            json = JSON.parse(xhttp.responseText);
+            //Loops through json
+            for(var i=0;i<json.length;i++){
+                AddMarker(json[i]);
+            }
+
+        }};
+    xhttp.open("GET", "Template.json", true);
+    xhttp.send();
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# NewsMap
+First Set up map--
+    For dark theme
+        style: 'mapbox://styles/mapbox/dark-v9'
+    for Light Theme 
+        style: 'mapbox://styles/mapbox/light-v9'
+
+
+API News---
+https://newsapi.org/s/bbc-news-api
